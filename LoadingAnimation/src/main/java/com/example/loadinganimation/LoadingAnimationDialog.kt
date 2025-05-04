05a2610bb923415cbeb41d989c0f604edf7f8ebb
OneFeiFan <3022504358@qq.com>
2025-05-04 14:22:46 +0800

    禁止默认的返回关闭操作,添加手动控制方案

--- LoadingAnimation/src/main/java/com/example/loadinganimation/LoadingAnimationDialog.kt
+++ LoadingAnimation/src/main/java/com/example/loadinganimation/LoadingAnimationDialog.kt
@@ -10,17 +10,16 @@ class LoadingAnimationDialog(context: Context) : Dialog(context) {
 
     // 委托给内部的 LoadingAnimationView
     private val contentView: LoadingAnimation
+    private var canClose: Boolean = true
 
     init {
         // 设置对话框无标题、透明背景等样式
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window?.setBackgroundDrawableResource(android.R.color.transparent)
+        setCancelable(false)
         // 初始化内容视图
         contentView = LoadingAnimation(context, null)
         setContentView(contentView)
-
-
-//        setCancelable(false)
     }
 
     // 将所有公共方法委托给 contentView
@@ -31,7 +30,17 @@ class LoadingAnimationDialog(context: Context) : Dialog(context) {
     fun setTextSize(textSize: Float) = contentView.setTextSize(textSize)
     fun setEnlarge(enlarge: Int) = contentView.setEnlarge(enlarge)
     fun setTextViewVisibility(isVisible: Boolean) = contentView.setTextViewVisibility(isVisible)
-
+    fun setCloseOnClick(canClose: Boolean) {
+        this.canClose = canClose
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if(canClose){
+            dismiss()
+        }else{
+            hide()
+        }
+    }
     // 如果需要覆盖show方法确保设置正确
 //    override fun show() {
 //        super.show()
